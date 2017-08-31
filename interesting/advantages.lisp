902afc33e8140325c915f6a91b96c2525456bd07
Andrii Lysenko <senkoily@gmail.com>
2017-08-31 15:10:40 +0300

    Refactor advantages.lisp adding some simple and good cases of lisp

--- interesting/advantages.lisp
+++ interesting/advantages.lisp
@@ -5,7 +5,7 @@
     (age 42))
 
 (defparameter lst (loop for name in '(alice bob john kate arthur)
-                        for age  in '(9 3 5 15 4)
+                        for age  in '(9 3 6 15 4)
                       collect (make-person :name name :age age)))
 
 ;; get the biggest age
@@ -16,6 +16,8 @@
 (reduce #'+ lst :key #'person-age)
 ;; find person with name alice
 (find 'alice lst :key #'person-name)
+;; find person of age 4
+(find 4 lst :key #'person-age)
 ;; get the oldest person
 (find (reduce #'max lst :key #'person-age)
        lst :key #'person-age)
@@ -28,8 +30,25 @@
 (defun age-valid? (age) (<= age 9))
 (remove-if-not #'age-valid? lst :key #'person-age)
 
+;; get a number of persons 9 y. o. or less
+(count-if #'age-valid? lst :key #'person-age)
+
+;; get everybody whose age is even
+(remove-if-not #'evenp lst :key #'person-age)
+
+;; get everybody whose age is odd
+(remove-if-not #'oddp lst :key #'person-age)
+
 ;; get everybody sorted according to their age in descending order
 (sort (copy-list lst) #'> :key #'person-age)
+
+;; get everybody sorted according to the name in ascending order
+(sort (copy-list lst) #'string<=
+    :key #'(lambda (p) (string (person-name p))))
+
+;; get everybody sorted according to length of the name in ascending order
+(sort (copy-list lst) #'<
+    :key #'(lambda (p) (length (string (person-name p)))))
 
 ;; for everybody who is more than 9 years old set age of 42
 (mapc #'(lambda (p)
@@ -41,3 +60,6 @@
 (make-list 7 :initial-element
           (make-person :name 'john#\ mccarthy
                        :age 90))
+
+;; sum of all days in each month of a year
+(apply #'+ '(31 28 31 30 31 30 31 31 30 31 30 31)) ; => 365
